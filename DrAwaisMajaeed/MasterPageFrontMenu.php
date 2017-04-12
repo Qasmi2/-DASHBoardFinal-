3830c42e8f112453bbefce489780c8beb63fce24
Qasmi2 <nadeemqasmi40@yahoo.com>
2017-04-12 12:52:07 +0500

    DeashBoard Final
    be countinues

---
+++ DrAwaisMajaeed/MasterPageFrontMenu.php
@@ -0,0 +1,69 @@
+
+
+ <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+                           
+      </button>
+        <a class="navbar-brand" href="index.php">Administration Cpanel</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+          <li class="active"><a href="index.php">Home</a></li>
+          <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Events
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+               
+            <li><a href="createEvent.php">Create Event  </a></li>
+            
+            <li><a href="others.php">Create Other Event </a></li>
+           
+          
+         
+        </ul>
+      </li>
+       <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Appeal 
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+               
+            <li><a href="bloodappeal.php">Blood Appeal  </a></li>
+            <li><a href="donationAppeal.php">Donation Appeal  </a></li>
+            <li><a href="foodAppeal.php">Food Appeal  </a></li>
+            <li><a href="otherappeal.php">other Appeal  </a></li>
+          
+         
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">User Management  
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Verified Users</a></li>
+          <li><a href="#">New Registered Users </a></li>
+           <li><a href="#">Role Assign to User </a></li>
+         
+        </ul>
+      </li>
+       <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">System Configuration 
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+             <li><a href="#">Database Operation </a></li>
+            <li><a href="#">User Verification</a></li>
+          <li><a href="#">Role Assign to User </a></li>
+           
+        </ul>
+      </li>
+      
+      
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
